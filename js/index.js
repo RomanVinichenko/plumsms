48467533eb80635c0cff005c5ce1301d234023fd
r.vinichenko <r.vinichenko@disoft.dev>
2022-12-20 03:30:08 +0200

    Updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-$(function () {});
+$(".terms__nav a, .footer a").on("click", function () {
+
+  let href = $(this).attr("href");
+
+  $("html, body").animate({
+    scrollTop: $(href).offset().top
+  }, {
+    duration: 370,
+    easing: "linear"
+  });
+
+  return false;
+});
