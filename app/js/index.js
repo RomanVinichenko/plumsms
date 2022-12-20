f5444ecc90c8e0096d3e72f492ed5b74546a533f
r.vinichenko <r.vinichenko@disoft.dev>
2022-12-20 17:50:42 +0200

    8

--- app/js/index.js
+++ app/js/index.js
@@ -11,3 +11,16 @@ $("a").on("click", function () {
 
   return false;
 });
+
+
+let header = document.querySelector(".header");
+
+window.addEventListener('scroll', function () {
+  let scrollPos = window.scrollY;
+
+  if (header && scrollPos > 10) {
+    header.classList.add("header--active");
+  } else {
+    header.classList.remove("header--active");
+  }
+});
